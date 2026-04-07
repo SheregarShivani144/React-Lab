c295b8e1ae415037b8943786d02c8fc3f15dbbad
SheregarShivani144 <shivani.23cs144@sode-edu.in>
2026-04-07 12:27:39 +0530

    Create contact.jsx

---
+++ lab8/contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+function Contact()
+{
+    return (
+    <div>
+    <h2>Contact Page</h2>
+     <p>Email:abc@gmail.com</p>
+     <p>1234567891</p>
+     <p>Github:abc@github</p>
+     </div>
+    );
+}
+export default Contact;
