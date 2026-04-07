3432f167790d72e23d783a367ee6e1d8011b894b
SheregarShivani144 <shivani.23cs144@sode-edu.in>
2026-04-07 12:26:45 +0530

    Create Home.jsx

---
+++ lab8/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+function Home()
+{
+    return (
+    <div>
+    <h2>Home Page</h2>
+    <p>Please navigate through different pages for more information</p>
+     </div>
+    );
+}
+export default Home;
