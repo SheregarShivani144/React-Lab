6ce9e45285809f59dc70e8e6c869f810798e7ef7
SheregarShivani144 <shivani.23cs144@sode-edu.in>
2026-04-07 12:28:17 +0530

    Create about.jsx

---
+++ lab8/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+function About() {
+    return (
+    <div>
+    <h2>About Page</h2>
+    <p>Name:Shivani</p>
+    <p>Education:B.E in CS</p>
+    </div>
+    );
+}
+export default About;
